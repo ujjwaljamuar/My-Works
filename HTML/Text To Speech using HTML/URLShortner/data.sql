751a6b4519aed2254d4c3b6625e492f11fea0f9b
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2021-04-16 19:57:09 +0530

    Delete HTML/Text To Speech using HTML directory

--- HTML/Text To Speech using HTML/URLShortner/data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE links (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-url TEXT(300) NOT NULL,
-title VARCHAR(8)
-);
-INSERT INTO links (`url`, `title`)
-    VALUES ('https://www.facebook.com/bikash1317/', 'Bikash Kumar Dash');
-    
-/* Import this file to your database created for url shortner. */
--- HTML/Text To Speech using HTML/URLShortner/setup.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'sql103.epizy.com'; // Your Host Name
-$dbuser = 'epiz_28382407'; // Database Username
-$dbpass = 'SQaPXbRXUAkHic'; // Database password
-$dbname = 'epiz_28382407_rdbi'; // Database Name
-$siteurl = 'http://rdbi.rf.gd/'; // Enter your website url without any slash(/) in the end
-$sitename = 'CODEX'; // Your Website Name in 8 or less words
-?>
